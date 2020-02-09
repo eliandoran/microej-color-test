774f198dae521dd0c8b18f90f5be42ab7b8a893f
Elian Doran <doran.adoris.elian@gmail.com>
2020-02-09 11:11:49 +0200

    Add constant for easy switching

--- src/xyz/doran/elian/ej/colortest/Main.java
+++ src/xyz/doran/elian/ej/colortest/Main.java
@@ -9,12 +9,16 @@ package xyz.doran.elian.ej.colortest;
 import ej.microui.MicroUI;
 import ej.mwt.Desktop;
 import ej.mwt.Panel;
+import ej.mwt.Widget;
+import xyz.doran.elian.ej.colortest.pages.BasicColorsPage;
 import xyz.doran.elian.ej.colortest.pages.PermutedBasicColorsPage;
 
 /**
  *
  */
 public class Main {
+
+	private static final boolean PERMUTE_COLORS = false;
 
 	/**
 	 * @param args
@@ -23,7 +27,14 @@ public class Main {
 		MicroUI.start();
 
 		Panel mainPanel = new Panel();
-		mainPanel.setWidget(new PermutedBasicColorsPage());
+
+		Widget widget;
+		if (PERMUTE_COLORS) {
+			widget = new PermutedBasicColorsPage();
+		} else {
+			widget = new BasicColorsPage();
+		}
+		mainPanel.setWidget(widget);
 
 		Desktop desktop = new Desktop();
 		mainPanel.showFullScreen(desktop);
